36bd79a67f29cbc735ba3b9683e1bdbf80c75164
chikage <35529908+chik4ge@users.noreply.github.com>
2025-12-30 08:13:06 +0900

    setup spring boot application for backend

---
+++ server/src/main/kotlin/me/chik4ge/tradukisto/TradukistoApplication.kt
@@ -0,0 +1,11 @@
+package me.chik4ge.tradukisto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TradukistoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TradukistoApplication>(*args)
+}
---
+++ server/src/test/kotlin/me/chik4ge/tradukisto/TradukistoApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.chik4ge.tradukisto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TradukistoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
